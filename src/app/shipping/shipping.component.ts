d02a084beada737cfa0283c9c84edcdc6fe82eb4
Brandon White <brandon.white@improving.com>
2019-09-12 11:04:12 -0400

    Use the cart service to retrieve shipping data and add link from cart to shipping component

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+  shippingCosts;
 
-  ngOnInit() {
+  constructor( private cartService: CartService ) { }
+
+  ngOnInit() {    
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
